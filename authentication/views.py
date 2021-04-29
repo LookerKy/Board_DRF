9952a1e89223fca4875a969718a52c54de8011c0
LookerKy <nagaraz07@gmail.com>
2021-04-29 16:11:11 +0900

    refresh token read setcookie

--- authentication/views.py
+++ authentication/views.py
@@ -1,26 +1,49 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from rest_framework.response import Response
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.serializers import TokenRefreshSerializer
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from rest_framework import permissions, status
 from .serializer import UserTokenSerializer, CustomUserSerializer
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 
+# @ TODO create Android token API
+
+
+class RefreshTokenView(TokenRefreshView):
+    serializer_class = TokenRefreshSerializer
+
+    def get(self, request):
+        if request.COOKIES:
+            print(request.COOKIES['refresh'])
+            serializer = self.get_serializer(data={'refresh': request.COOKIES['refresh']})
+            try:
+                serializer.is_valid(raise_exception=True)
+                print(serializer.validated_data)
+            except TokenError as e:
+                raise InvalidToken(e.args[0])
+            response = JsonResponse({"access": serializer.validated_data["access"]}, status=status.HTTP_200_OK)
+            response.set_cookie('refresh', serializer.validated_data["refresh"], httponly=True)
+            return response
+        else:
+            return Response({"message": "failure"}, status=status.HTTP_401_UNAUTHORIZED)
+
 
 class UserTokenView(TokenObtainPairView):
     serializer_class = UserTokenSerializer
 
     def post(self, request, *args, **kwargs):
+        print(type(request.data))
         serializer = self.get_serializer(data=request.data)
         # print(serializer)
         try:
             serializer.is_valid(raise_exception=True)
         except TokenError as e:
             raise InvalidToken(e.args[0])
-        print(serializer.validated_data["refresh"])
 
-        # @TODO get Device setCookie Or Just Json
+        # print(serializer.validated_data["refresh"])
+        # Response(serializer.validated_data, status=status.HTTP_200_OK)
         response = JsonResponse({"access": serializer.validated_data["access"]}, status=status.HTTP_200_OK)
         response.set_cookie('refresh', serializer.validated_data["refresh"], httponly=True)
         return response
