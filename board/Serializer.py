a4658b11c4d0dadf90cfbe3d9724076052a011c5
LookerKy <nagaraz07@gmail.com>
2021-05-13 19:00:10 +0900

    add BoardSerializer

--- board/Serializer.py
+++ board/Serializer.py
@@ -1,1 +1,8 @@
 from rest_framework import serializers
+from .models import BoardModel
+
+
+class BoardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BoardModel
+        fields = '__all__'
