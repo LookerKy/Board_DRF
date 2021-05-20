d1969caf0dae5d9dc415e2b8376d90e407da2228
LookerKy <nagaraz07@gmail.com>
2021-05-20 18:55:54 +0900

    add serializer

--- board/serializer.py
+++ board/serializer.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
-from .models import BoardModel
+from .models import BoardModel, TopicModel
 
 
-class BoardSerializer(serializers.ModelSerializer):
+class BoardListSerializer(serializers.ModelSerializer):
     class Meta:
         model = BoardModel
         fields = '__all__'
+
+
+class TopicSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = TopicModel
+        fields = '__all__'
+
+
+class TopicListSerializer(serializers.ModelSerializer):
+    topics = TopicSerializer(many=True, required=False)
+
+    class Meta:
+        model = BoardModel
+        fields = '__all__'
+
+
+class PostSerializer(serializers.ModelSerializer):
+    pass
